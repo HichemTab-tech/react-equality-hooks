462653968931a27411c02676155092303bc183e1
HichemTab-tech <konanhichemsinshi@gmail.com>
2025-09-28 12:06:50 +0100

    Add demo

--- demo/app.tsx
+++ demo/app.tsx
@@ -1,17 +1,45 @@
 import * as React from 'react';
-//import {GiveHichemTabTechCredit, MyComponent} from '../dist/main.js'; // Import your built library
-import {GiveHichemTabTechCredit, MyComponent} from 'react-equality-hooks'; // Import your dev library
+import {useMemo} from 'react-equality-hooks';
+import {useState, useMemo as originalUseMemo} from "react";
 
 const App = () => {
 
-    GiveHichemTabTechCredit();
+    const [dep, setDep] = useState({
+        v: 0,
+    });
+
+    const [trigger, setTrigger] = useState(0);
+
+    const value = useMemo(() => Math.random(), [dep]);
+
+    const secondValue = originalUseMemo(() => Math.random(), [dep]);
+
+    const handle = (change: boolean) => {
+        if (change) {
+            setDep({
+                ...dep,
+                v: dep.v+1
+            })
+        }
+        else{
+            setDep({...dep})
+        }
+    }
 
     return (
         <div>
-            <h1 className="text-red-600">React equality hooks Demo</h1>
-            <MyComponent/>
+            <h1>Smart useMemo</h1>
+            <h3>Deps: {JSON.stringify(dep)}</h3>
+            <h4>Value using react-equality-hooks's useMemo: {value}</h4>
+            <h4>Value using react useMemo: {secondValue}</h4>
+            <br/>
+            <h4>Normal value: {Math.random()}</h4>
+            <br/>
+            <button onClick={() => handle(true)}>Change object</button>
+            <button onClick={() => handle(false)}>refresh object</button>
+            <button onClick={() => setTrigger(trigger+1)}>trigger re-render</button>
         </div>
-    );
+    )
 };
 
 export default App;
