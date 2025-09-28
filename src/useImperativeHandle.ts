fa365e5ad194171dd81699a687518767b7d7ddcc
HichemTab-tech <konanhichemsinshi@gmail.com>
2025-09-28 11:43:59 +0100

    refactor: rename init to createHandle in useImperativeHandle functions

--- src/useImperativeHandle.ts
+++ src/useImperativeHandle.ts
@@ -1,18 +1,18 @@
 import {useImperativeHandle as originalUseImperativeHandle, type Ref, type DependencyList} from "react";
 import useStableValue, {type Comparator, type Strategy} from "./hooks/useStableValue";
 
-export function useImperativeHandle<T, R extends T>(ref: Ref<T> | undefined, init: () => R, deps?: DependencyList, comparison: Strategy|Comparator = "deep") {
+export function useImperativeHandle<T, R extends T>(ref: Ref<T> | undefined, createHandle: () => R, deps?: DependencyList, comparison: Strategy|Comparator = "deep") {
     const changedDeps = useStableValue(deps, comparison);
 
-    return originalUseImperativeHandle(ref, init, changedDeps);
+    return originalUseImperativeHandle(ref, createHandle, changedDeps);
 }
 
-export function useImperativeHandleDeep<T, R extends T>(ref: Ref<T> | undefined, init: () => R, deps?: DependencyList) {
-    return useImperativeHandle(ref, init, deps, "deep");
+export function useImperativeHandleDeep<T, R extends T>(ref: Ref<T>|undefined, createHandle: () => R, deps: DependencyList) {
+    return useImperativeHandle(ref, createHandle, deps, "deep");
 }
-export function useImperativeHandleShallow<T, R extends T>(ref: Ref<T> | undefined, init: () => R, deps?: DependencyList) {
-    return useImperativeHandle(ref, init, deps, "shallow");
+export function useImperativeHandleShallow<T, R extends T>(ref: Ref<T>|undefined, createHandle: () => R, deps: DependencyList) {
+    return useImperativeHandle(ref, createHandle, deps, "shallow");
 }
-export function useImperativeHandleIdentity<T, R extends T>(ref: Ref<T> | undefined, init: () => R, deps?: DependencyList) {
-    return useImperativeHandle(ref, init, deps, "identity");
+export function useImperativeHandleIdentity<T, R extends T>(ref: Ref<T>|undefined, createHandle: () => R, deps: DependencyList) {
+    return useImperativeHandle(ref, createHandle, deps, "identity");
 }
